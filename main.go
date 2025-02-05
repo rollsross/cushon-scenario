4ca2504625147dab1ae0669192350b1fe3f7e2d5
rollsross <130713359+rollsross@users.noreply.github.com>
2025-02-05 13:32:38 +0000

    creates server

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	port := "8080"
+
+	fmt.Printf("server is running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
