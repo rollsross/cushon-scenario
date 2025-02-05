ffe6f036fb51e812b07d0f1538dbaae6045069e4
rollsross <130713359+rollsross@users.noreply.github.com>
2025-02-05 13:34:52 +0000

    adds sqlite

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	mux := http.NewServeMux()
 
 	port := "8080"
